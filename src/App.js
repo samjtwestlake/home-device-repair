ef91c0a04b99490821e0785d31139eb0a619fef0
samjtwestlake <samjtwestlake@gmail.com>
2022-07-05 14:07:11 +0100

    bug fixing

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Outlet } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <img src={logo} className="App-logo" alt="logo" />
       <p>
         Deployment working! :)
