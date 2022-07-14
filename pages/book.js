8ad345a7c36fda4682d552a48ccbb093aaa631fa
samjtwestlake <samjtwestlake@gmail.com>
2022-07-14 16:40:24 +0100

    test

--- pages/book.js
+++ pages/book.js
@@ -3,7 +3,7 @@ import { Calendar, Col, Row, Card } from "antd";
 const Book = () => {
   return (
     <>
-      <div>book</div>
+      <div>book test</div>
       <Row>
         <Col span={22} offset={1}>
           <Calendar />
